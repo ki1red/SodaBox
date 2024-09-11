960936bad18e601e7b3fdb2e3eba4d7d68464f76
ki1red <druzhinin_nikita@list.ru>
2024-09-11 22:14:33 +0700

    Добавьте файлы проекта.

---
+++ SodaBox/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SodaBox.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
